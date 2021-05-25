43e6b5a92182b1aed3923f93f81bb864f6d39313
Preston <prestonj@uga.edu>
2021-05-24 21:27:27 -0400

    dumb fix

--- src/Labels.cpp
+++ src/Labels.cpp
@@ -6,27 +6,28 @@ namespace Abstract
 /************************************************
  *  CONSTRUCTORS/DESTRUCTORS
  ***********************************************/
-inline Labels::Labels() :
+Labels::Labels() :
 		name("INCORRECT LABEL CONSTRUCTOR")
 {
 }
 
-inline Labels::Labels(std::string name) :
+Labels::Labels(std::string name) :
 		name(name)
 {
 }
 
-inline Labels::Labels(std::vector<std::string> labels) :
+Labels::Labels(std::vector<std::string> labels) :
 		name("NO_NAME"), labels(labels)
 {
 }
 
-inline Labels::Labels(std::string name, std::string label) :
-		name(name), labels({label})
+Labels::Labels(std::string name, std::string label) :
+		name(name), labels(
+		{ label })
 {
 }
 
-inline Labels::Labels(std::string name, std::vector<std::string> labels) :
+Labels::Labels(std::string name, std::vector<std::string> labels) :
 		name(name), labels(labels)
 {
 }
@@ -39,29 +40,29 @@ Labels::~Labels()
  *  GETTER/SETTER
  ***********************************************/
 
-inline std::string Labels::getName() const
+std::string Labels::getName() const
 {
 	return this->name;
 }
 
-inline std::string Labels::getLabel() const
+std::string Labels::getLabel() const
 {
 	if (this->labels.empty())
 		return "";
 	return this->labels.back();
 }
 
-inline std::vector<std::string> Labels::getLabels() const
+std::vector<std::string> Labels::getLabels() const
 {
 	return this->labels;
 }
 
-inline void Labels::setName(std::string name)
+void Labels::setName(std::string name)
 {
 	this->name = name;
 }
 
-inline void Labels::setLabels(std::string label)
+void Labels::setLabels(std::string label)
 {
 	this->labels.clear();
 	this->addLabel(label);
@@ -76,12 +77,12 @@ inline void Labels::setLabels(std::vector<std::string> labels)
  *  MUTATORS
  ***********************************************/
 
-inline void Labels::addLabel(std::string label)
+void Labels::addLabel(std::string label)
 {
 	this->labels.push_back(label);
 }
 
-inline void Labels::addLabels(std::vector<std::string> labels)
+void Labels::addLabels(std::vector<std::string> labels)
 {
 	this->labels.insert(this->labels.end(), labels.begin(), labels.end());
 }
@@ -90,7 +91,7 @@ inline void Labels::addLabels(std::vector<std::string> labels)
  *  FUNCTIONS
  ***********************************************/
 
-inline bool Labels::containsLabel(const std::string query)
+bool Labels::containsLabel(const std::string query)
 {
 	for (std::string currLabel : this->labels)
 	{
@@ -101,38 +102,38 @@ inline bool Labels::containsLabel(const std::string query)
 	return false;
 }
 
-inline bool Labels::compareLabels(const std::vector<std::string> otherLabels)
+bool Labels::compareLabels(const std::vector<std::string> otherLabels)
 {
 	return this->compareLabels(otherLabels, 1);
 }
 
 //copy constructor
-inline Labels::Labels(const Labels &rhs) :
+Labels::Labels(const Labels &rhs) :
 		name(rhs.name), labels(rhs.labels)
 {
 
 }
 //move constructor
-inline Labels::Labels(Labels &&rhs) :
+Labels::Labels(Labels &&rhs) :
 		name(rhs.name), labels(rhs.labels)
 {
 }
 //copy assignment
-inline Labels& Labels::operator =(const Labels &rhs)
+Labels& Labels::operator =(const Labels &rhs)
 {
 	this->name = rhs.name;
 	this->labels = rhs.labels;
 	return *this;
 }
 //move assignment
-inline Labels& Labels::operator =(Labels &&rhs)
+Labels& Labels::operator =(Labels &&rhs)
 {
 	this->name = rhs.name;
 	this->labels = rhs.labels;
 	return *this;
 }
 
-inline bool Labels::compareLabels(const std::vector<std::string> otherLabels,
+bool Labels::compareLabels(const std::vector<std::string> otherLabels,
 		unsigned int numMatches)
 {
 	unsigned int matchCounter = 0;
